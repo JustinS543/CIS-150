87770fc6e2b3a70605156b7ce99491eb10c47916
morpheeus543 <A543432321234@gmail.com>
2022-09-07 11:40:38 -0400

    end of class

--- C++/CIS_09_07_22_cout_variables.cpp
+++ C++/CIS_09_07_22_cout_variables.cpp
@@ -1,7 +1,7 @@
 //=========================
 //Author: Justin Schley
 //Date:   09-07-2022
-//Description: cout and veriable definitions
+//Description: cout and variable definitions
 //=========================
 
 
@@ -26,7 +26,45 @@ int main()
     //========================================================
     //These will both print together on the same line (combining two strings)
     //========================================================
-    cout << "Hello this is a test.\n" << "This is another test.";
+    // cout << "Hello this is a test. " << "This is another test.";
+
+    //========================================================
+    //looking at variables
+    //========================================================    
+
+    //This is a variable definition. 
+    //int number;
+
+    //This is a variable assignment. This gives a value to the defined RAM address.
+    //number = 5;
+
+    //output a string literal and the value stored in the variable num
+
+    //cout << "number = " << number << endl;
+
+    //reassignment to the variable number
+
+    //number = 99;
+
+    //cout << "number = " << number << endl;
+
+    //Always declare variable before calling
+    int number = 25;
+    cout << "number = " << number << endl;
+    
+    int anotherNumber = 25;
+    cout << "anotherNumber = " << anotherNumber << endl;
+
+    //largest values for int
+    int maxNum = 2147483647;
+    cout << maxNum << endl;
+    
+    //higher than largest int val
+    int maxNum2 = 2147483648;
+    cout << maxNum2 << endl;
+
+
+
 
     return 0;
 }
