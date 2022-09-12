2254b0d1ca6d5c55998d7ef3f7066416326d7ab1
morpheeus543 <A543432321234@gmail.com>
2022-09-12 11:20:49 -0400

    9-12

---
+++ 9-12-22/main.cpp
@@ -0,0 +1,35 @@
+//==========================================================================
+//Author: Justin Schley
+//Date  : 9-12-22
+//Desc  : output variables, concatentation, math operations, and user input
+//==========================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //=============================
+    //Looking at char datatype
+    //=============================
+
+    // char letter = 'A';
+    // int asciiValueOfLetter = letter;
+    // cout << letter << endl;
+    // cout << letter << " has an ascii value of: " << asciiValueOfLetter << endl;
+
+    //=============================
+    //Looking at string datatype
+    //=============================
+
+    int age = 29;
+    string fName = "Shannon";
+    string lName = "Thomas";
+    int currentYear = 2022;
+    int yearBorn = currentYear - age;
+
+    cout << "Hello " << fName << " " << lName << "!" << endl;
+    cout << "You are " << age << " years old and born in " << yearBorn << "." << endl;
+
+}
