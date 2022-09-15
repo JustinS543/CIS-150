a94ba4c8436fe05697499a161a666858ccaa9fee
morpheeus543 <A543432321234@gmail.com>
2022-09-15 09:09:03 -0400

    gitignore

--- 9-14-22/main.cpp
+++ 9-14-22/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 int main()
 {
 
-    
+    int x;
+    int y;
+    int add;
+    int subtr;
+    int mult;
+    int divide;
+
+
+    cout << "Please enter a number: " << endl;
+    cin >> x;
+    cout << "Please enter another number: " << endl;
+    cin >> y;
+
+    add = x + y;
+    subtr = x - y;
+    mult = x * y;
+    divide = x / y;
+
+    cout << x << " + " << y << " = " << add << endl;
+    cout << x << " - " << y << " = " << subtr << endl;
+    cout << x << " * " << y << " = " << mult << endl;
+    cout << x << " / " << y << " = " << divide << endl;
+
+
+
 
 }
