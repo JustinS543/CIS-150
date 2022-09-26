05b5b3b22a993a75b4d13d7a69ea9bba0c443ee0
Justin Schley <A543432321234@gmail.com>
2022-09-26 11:46:34 -0400

    9-26

---
+++ 2022-09-21/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // int favnum;
+    // string fullname;
+
+    // cout << "Enter your favorite number: " << endl;
+    // cin >> favnum;
+    // cin.ignore();
+    // cout << "Enter your full name: " << endl;
+    // getline(cin, fullname);
+    // cout << "Hello " << fullname << ", you entered " << favnum << endl;
+
+    // string word;
+    // int wlength;
+    // char lastletter;
+
+    // cout << "Enter a word: " << endl;
+    // cin >> word;
+    // wlength = word.length() - 1;
+    // lastletter = word.at(wlength);
+    // cout << "The last letter in " << word << " is " << lastletter << endl;
+
+    
+
+}
