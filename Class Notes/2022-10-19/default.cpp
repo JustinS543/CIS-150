5a2145a2fd4e628cb41fbc62b638d7f735505294
Justin Schley <A543432321234@gmail.com>
2022-10-19 11:10:48 -0400

    2022-10-19

---
+++ Class Notes/2022-10-19/default.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //decrement loops are good for displaying something backwards
+    //counters must be initialized before the loop (assign a value to it)
+
+    //do-while loop
+    //do-while is post test, while is pre test
+    //do while is great for menu driven programs
+    //do whiles require semicolon after the while part
+
+    //
+
+    /*
+
+    do 
+    {
+        statement
+    } while(condition);
+
+    */
+
+
+    /*
+    string word;
+    int i = 0;
+    int wordlen;
+
+    cout << "Enter a word: ";
+    cin >> word;
+    wordlen = word.length();
+
+    while(i < wordlen)
+    {
+        cout << word.at(i) << endl;
+        i++;
+    }
+    */
+
+    
+
+
+    //for loop
+
+    /*
+    
+    for(initialization; test; update)
+    {
+        statement;
+    }
+
+    //for example
+    
+    for(count = 1; count <= 5; count++)
+    {
+        cout << "hello" << endl;
+    }
+
+    for(int i = 0; i< 5; i++)
+    {
+        cout << i << endl;
+    }
+
+    if i is defined inside of a for loop, then it can only be used inside the loop
+
+    can init multiple items:
+        for(x = 1, y = 1; x < 5; x++)
+
+    
+
+    */
+
+
+
+
+}
