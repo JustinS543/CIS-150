3f18ffe8f2010da9765e6aecae278f8a443ae5ae
Justin Schley <A543432321234@gmail.com>
2022-11-08 11:44:52 -0500

    Homework 05

---
+++ Homework/Homework 5/default.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int addition(int num1, int num2)
+{
+    return (num1 + num2);
+}
+
+int subtract(int num1, int num2)
+{
+    return (num1 - num2);
+}
+
+int multiply(int num1, int num2)
+{
+    return (num1 * num2);
+}
+
+int divide(int num1, int num2)
+{
+    return (num1 / num2);
+}
+
+int absolute(int num1)
+{
+    if(num1 < 0)
+    {
+        num1 = num1 * -1;
+    }
+
+    else if(num1 >= 0)
+    {
+        //do nothing
+    }
+    return num1;
+}
+
+void welcome()
+{
+    cout << "M A T H   A P P L I C A T I O N" << endl;
+    cout << "===============================" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Division" << endl;
+    cout << "4. Multiplication" << endl;
+    cout << "5. Absolute Value" << endl;
+}
+
+string selection()
+{
+    string choice;
+    bool valin = false;
+    cout << "Please make a selection from the choices above: ";
+    cin >> choice;
+    while(valin == false)
+    {
+        if(choice >= "0" && choice <= "9")
+        {
+            valin = true;
+        }
+        else
+        {
+            cout << "INVALID INPUT. TRY AGAIN: ";
+            cin >> choice;
+        }
+    }
+    return choice;
+}
+
+int input1()
+{
+    int input1;
+    cout << "Enter a number:";
+    cin >> input1;
+    return input1;
+}
+
+int input2()
+{
+    int input2;
+    cout << "Enter a number:";
+    cin >> input2;
+    return input2;
+}
+
+void display(int num1)
+{
+    int answer;
+    answer = absolute(num1);
+    cout << "|" << num1 << "|" << " = " << answer << endl;
+}
+
+void display(int num1, int num2, string choice)
+{
+    if(choice == "1")
+    {
+        int answer;
+        answer = addition(num1, num2);
+        cout << num1 << " + " << num2 << " = " << answer << endl;
+    }
+    if(choice == "2")
+    {
+        int answer;
+        answer = subtract(num1, num2);
+        cout << num1 << " - " << num2 << " = " << answer << endl;
+    }
+    
+    if(choice == "3")
+    {
+        int answer;
+        answer = divide(num1, num2);
+        cout << num1 << " / " << num2 << " = " << answer << endl;
+    }
+    if(choice == "4")
+    {
+        int answer;
+        answer = multiply(num1, num2);
+        cout << num1 << " * " << num2 << " = " << answer << endl;
+    }
+}
+
+string gameover()
+{
+    string input;
+    bool valin = false;
+    cout << "Would you like to run again? (1 - Yes OR 2 - No): ";
+    cin >> input;
+    while(valin == false)
+    {
+        if(input == "1" || input == "2")
+        {
+            valin = true;
+            return input;
+        }
+        else
+        {
+            cout << "INVALID INPUT. TRY AGAIN: ";
+            cin >> input;
+        }
+    }
+}
+
+int main()
+{
+    string choice;
+    string gametest;
+    int in1;
+    int in2;
+    bool endgame = false;
+
+    while(endgame == false)
+    {
+        system("clear");
+        welcome();
+        choice = selection();
+        if(choice == "5")
+        {
+            in1 = input1();
+            display(in1);
+        }
+        else
+        {
+            in1 = input1();
+            in2 = input2();
+            display(in1, in2, choice);
+        }
+        gametest = gameover();
+        if(gametest == "1")
+        {
+            //keep going
+        }
+        else if(gametest == "2")
+        {
+            endgame = true;
+            cout << "Program completed." << endl;
+        }
+    }
+}
